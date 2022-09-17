01edac5754f163358592b0d13e03e515cff966f1
Luis David Escobedo Velasquez <lescobedo.im@gmail.com>
2022-09-16 22:17:58 -0500

    solucionado_errores

--- ahorcado.js
+++ ahorcado.js
@@ -12,21 +12,21 @@ const palabras = [
     'APLICACION',
     'JAVASCRIPT'
 ];
-const btn = id('jugar');
-const imagen = id( 'imagen' );
+const btn = document.getElementById('jugar');
+const imagen = document.getElementById( 'imagen' );
 const btn_letras = document.querySelectorAll( "#letras button" );
 
 /* click en iniciar juego */
 btn.addEventListener('click', iniciar );
 
 function iniciar(event){
-    id('resultado').innerHTML = '';
+    document.getElementById('resultado').innerHTML = '';
     imagen.src = 'img/img0.png';
     btn.disabled = true;
     cant_errores = 0;
     cant_aciertos = 0; 
 
-    const parrafo = id( 'palabra_a_adivinar' );
+    const parrafo = document.getElementById( 'palabra_a_adivinar' );
     parrafo.innerHTML = ''; 
 
     const cant_palabras = palabras.length;
@@ -78,11 +78,11 @@ function click_letras(event){
     }
 
     if( cant_errores == 9 ){
-        id('resultado').innerHTML ="Perdiste, la palabra era " + objetivo;
+        document.getElementById('resultado').innerHTML ="Perdiste, la palabra era " + objetivo;
         game_over( );
         alert("Fin del juego")
     }else if( cant_aciertos == objetivo.length ){
-        id('resultado').innerHTML = "¡Ganaste, Felicidades!";
+        document.getElementById('resultado').innerHTML = "¡Ganaste, Felicidades!";
         game_over( );
     }
     console.log( "la letra " + letra + " en la palabra " + palabra + " ¿existe?: " + acerto );
@@ -98,5 +98,8 @@ function game_over( ){
     btn.disabled = false;
 }
 
+function obtener_random( num_min, num_max){
+    return Math.floor( Math.random() * (num_max - num_min)) + num_min;
+}
 
 game_over( );
