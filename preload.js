aa885e2d17f7230df4ac3972677cb11fcb3d1980
Luis David Escobedo Velasquez <lescobedo.im@gmail.com>
2022-09-16 22:18:15 -0500

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-function id(str){
-    return document.getElementById(str);
-}
-
-function obtener_random( num_min, num_max){
-    return Math.floor( Math.random() * (num_max - num_min)) + num_min; /* 5 - 15 = 10 + 5 */
-}
